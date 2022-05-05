9c915f216cee1db464944258660762b6b6c89238
zyx <zxl082410@163.com>
2022-05-05 15:05:49 +0800

    Coder_6搭建结构和全局配置

---
+++ pages/home/home.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<!-- 轮播图 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item>
+				<view class="swiper-item">1111</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item">222</view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+	swiper {
+		height: 330rpx;
+		background-color: #007AFF;
+	}
+</style>
